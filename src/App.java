95edf32f372a1b8fcc8bcf6beb8946f798aef320
Che0807 <elpin0428@naver.com>
2025-04-16 19:20:56 +0900

    c기호char자료형으로변환

--- src/App.java
+++ src/App.java
@@ -4,11 +4,9 @@ public class App {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-
         int result = 0;
-
         String continuePrompt;
+
         do {
             System.out.println("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
@@ -17,20 +15,17 @@ public class App {
             int num2 = sc.nextInt();
 
             System.out.println("사칙연산 기호를 입력하세요: ");
-            String mathsymbols = sc.next();
-            if (mathsymbols.equals("+")) {
-                result = num1 + num2;
-            } else if (mathsymbols.equals("-")) {
-                result = num1 - num2;
-            } else if (mathsymbols.equals("*")) {
-                result = num1 * num2;
-            } else if (mathsymbols.equals("/")) {
-                result = num1 / num2;
-            } else System.out.println("옳 바른 연산 기호와 정수를 입력해주세요");
+            char mathSymbol = sc.next().charAt(0); // 문자열 → 첫 글자만 char로 저장
+
+            if (mathSymbol == '+') result = num1 + num2;
+            else if (mathSymbol == '-') result = num1 - num2;
+            else if (mathSymbol == '*') result = num1 * num2;
+            else if (mathSymbol == '/') result = num1 / num2;
+            else System.out.println("올바른 연산 기호를 입력해주세요!");
+
             System.out.println("결과: " + result);
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             continuePrompt = sc.next();
         } while (!continuePrompt.equals("exit"));
     }
 }
-
