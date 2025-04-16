d537aee5dd88a21996719210b2b8537fa4e0d050
Che0807 <elpin0428@naver.com>
2025-04-16 19:32:52 +0900

    Revert "나누기정산오류표시"
    This reverts commit 1db03f3337a8881bf8a7ca554443f35a71c01fe1.

--- src/App.java
+++ src/App.java
@@ -24,9 +24,9 @@ public class App {
             else if (mathSymbol == '/') {
                 if (num2 == 0) {
                     System.out.println("오류: 0으로는 나눌 수 없습니다. 다시 시도해주세요.");
-                }else
-                    result = num1 / num2;
                 }
+                result = num1 / num2;
+            }
             System.out.println("결과: " + result);
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             continuePrompt = sc.next();
