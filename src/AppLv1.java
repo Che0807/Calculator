1b6c7856bd827104b62c0f166c20252988c1caac
Che0807 <elpin0428@naver.com>
2025-04-16 15:57:37 +0900

    ãSc연산연산식진행식및결과추출

--- src/AppLv1.java
+++ src/AppLv1.java
@@ -2,17 +2,29 @@ import java.util.Scanner;
 
 public class AppLv1 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt();
+
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
+
         System.out.println("사칙연산 기호를 입력하세요: ");
         String mathsymbols = sc.next();
 
-        System.out.println("mathsymbols + = "+ num1 + mathsymbols + num2 );
+        int result = 0;
+        if (mathsymbols.equals("+")) {
+            result = num1 + num2;
+        } else if (mathsymbols.equals("-")) {
+            result = num1 - num2;
+        } else if (mathsymbols.equals("*")) {
+            result = num1 * num2;
+        }else if (mathsymbols.equals("/")) {
+            result = num1 / num2;
+        }else System.out.println("옳 바른 연산 기호와 정수를 입력해주세요");
 
-
+        System.out.println("결과: " + result);
     }
 }
