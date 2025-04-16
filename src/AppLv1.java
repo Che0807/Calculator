2b1cfc6bb7b30cf53f7871b108a7905c419e2df2
Che0807 <elpin0428@naver.com>
2025-04-16 17:53:34 +0900

    종료오류해결

--- src/AppLv1.java
+++ src/AppLv1.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class App {
+public class AppLv1 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+
         int result = 0;
 
         String continuePrompt;
-
         do {
             System.out.println("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
@@ -27,12 +27,12 @@ public class App {
             } else if (mathsymbols.equals("/")) {
                 result = num1 / num2;
             } else System.out.println("옳 바른 연산 기호와 정수를 입력해주세요");
-
             System.out.println("결과: " + result);
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             continuePrompt = sc.next();
-
         } while (continuePrompt.equals("exit"));
+
+
     }
 }
 
