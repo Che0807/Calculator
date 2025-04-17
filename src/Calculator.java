4e32b5f3fc74b71622dcc862fb6965366b58dd3c
Che0807 <elpin0428@naver.com>
2025-04-17 19:19:33 +0900

    캡슐화

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    List<Integer> resultList = new ArrayList<>();
+
+    private List<Integer> resultList = new ArrayList<>();
 
     public int calculate(int num1, int num2, char mathSymbol) {
-
         int result = 0;
         boolean isCalculated = true;
 
@@ -17,7 +17,7 @@ public class Calculator {
             result = num1 * num2;
         } else if (mathSymbol == '/') {
             if (num2 == 0) {
-                System.out.println("오류: 0으로는 나눌 수 없습니다. 다시 시도해주세요.");
+                System.out.println("오류: 0으로는 나눌 수 없습니다.");
                 isCalculated = false;
             } else {
                 result = num1 / num2;
@@ -28,10 +28,21 @@ public class Calculator {
         }
 
         if (isCalculated) {
-            System.out.println("결과: " + result);
+            System.out.println("계산 결과: " + result);
             resultList.add(result);
         }
 
         return result;
     }
+
+    public List<Integer> getResultList() {
+        return resultList;
+    }
+
+    public void setResultList(List<Integer> resultList) {
+        this.resultList = resultList;
+    }
 }
+
+
+
