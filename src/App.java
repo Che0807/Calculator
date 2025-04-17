332490474e39c1ddd97161d59ee37038858a1eb6
Che0807 <elpin0428@naver.com>
2025-04-17 17:15:05 +0900

    App에 Calculator 메서드 호출

--- src/App.java
+++ src/App.java
@@ -20,14 +20,10 @@ public class App {
             System.out.println("사칙연산 기호를 입력하세요: ");
             char mathSymbol = sc.next().charAt(0);
 
-            if (mathSymbol == '+') result = num1 + num2;
-            else if (mathSymbol == '-') result = num1 - num2;
-            else if (mathSymbol == '*') result = num1 * num2;
-            else if (mathSymbol == '/') {
-                if (num2 == 0) {
-                    System.out.println("오류: 0으로는 나눌 수 없습니다. 다시 시도해주세요.");
-                } else
-                    result = num1 / num2;
+            int result = calculator.calculate(num1, num2, mathSymbol);
+
+            if (result != 0 || mathSymbol == '/') {
+                System.out.println("결과: " + result);
             }
             System.out.println("결과: " + result);
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
